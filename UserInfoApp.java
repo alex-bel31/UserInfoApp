2db9a228e06557064413532b2699b239d50ebdcc
ваше имя <alex.bel7731@gmail.com>
2024-01-22 19:16:06 +0300

    created class UserInfoApp

---
+++ UserInfoApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UserInfoApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите данные (Фамилия Имя Отчество дата рождения номер телефона пол(m/f), разделяя пробелами):");
+        String input = scanner.nextLine();
+
+        try {
+            UserInfoProcessor.processInput(input);
+            System.out.println("Данные введены верно и записаны в файл.");
+        } catch (InputFormatException | FileHandlingException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+    }
+}
